e03680183474c0eff01217e8e1a86fa0d8576681
unknown <Jossuha@hotmail.fr>
2023-10-13 20:37:46 +0200

    modif start server

--- Start Server.bat
+++ Start Server.bat
@@ -1,18 +1,10 @@
 @echo off
 title KaoBot
 
-REM Vérifie si le dossier .git existe
-IF NOT EXIST .git (
-    REM Si le dossier .git n'existe pas, initialise un nouveau dépôt Git
-    "git_portable\git-bash.exe" -c "git init -b main"
-    "git_portable\git-bash.exe" -c "git remote add origin https://github.com/ShalltearFR/TwitchBot-3.0.git"
-) ELSE (
-    REM Si le dossier .git existe, effectue un git pull
-    "git_portable\git-bash.exe" -c "git pull https://github.com/ShalltearFR/TwitchBot-3.0.git"
-    pause
-    REM Démarrer le serveur Node.js
-    node.exe server.js
-)
+echo "Recherche de mise a jour"
+"git_portable\git-bash.exe" -c "git pull https://github.com/ShalltearFR/TwitchBot-3.0.git"
 
+echo "Demarrage du bot"
+node.exe server.js
 
 pause
