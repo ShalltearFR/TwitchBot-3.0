4ec4fad31419317a707c4225604db685a27d88f0
unknown <Jossuha@hotmail.fr>
2023-10-13 20:53:31 +0200

    better update

--- Start Server.bat
+++ Start Server.bat
@@ -1,10 +1,8 @@
 @echo off
 title KaoBot
 
-echo "Recherche de mise a jour"
-"git_portable\git-bash.exe" -c "git pull https://github.com/ShalltearFR/TwitchBot-3.0.git"
+"git_portable\git-bash.exe" -c --hide "git pull https://github.com/ShalltearFR/TwitchBot-3.0.git"
 
-echo "Demarrage du bot"
 node.exe server.js
 
 pause
