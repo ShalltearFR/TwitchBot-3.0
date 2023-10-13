86e2ff5a51ffab6a612561163ab7e8f743f6d255
unknown <Jossuha@hotmail.fr>
2023-10-13 20:21:32 +0200

    add git portable for auto update

--- Start Server.bat
+++ Start Server.bat
@@ -1,5 +1,18 @@
 @echo off
-title =KaoBot
-node.exe server.js
+title KaoBot
+
+REM Vérifie si le dossier .git existe
+IF NOT EXIST .git (
+    REM Si le dossier .git n'existe pas, initialise un nouveau dépôt Git
+    "git_portable\git-bash.exe" -c "git init -b main"
+    "git_portable\git-bash.exe" -c "git remote add origin https://github.com/ShalltearFR/TwitchBot-3.0.git"
+) ELSE (
+    REM Si le dossier .git existe, effectue un git pull
+    "git_portable\git-bash.exe" -c "git pull https://github.com/ShalltearFR/TwitchBot-3.0.git"
+    pause
+    REM Démarrer le serveur Node.js
+    node.exe server.js
+)
+
 
 pause
